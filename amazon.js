09e1bc35b110298e7389e97e626db99fed93cac3
Preston Carroll <plutusinc1@yahoo.com>
2020-01-10 18:49:07 -0600

    setting up mysql connection and inquirer

--- amazon.js
+++ amazon.js
@@ -1,1 +1,25 @@
-test1//testing
+//requiring mysql and inquirer
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: ""
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
